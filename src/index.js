b987e0107aa258ab9a34bae97e0b642a28dc0197
Bob Habig <rfhab2@gmail.com>
2018-02-04 03:21:20 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -10,3 +10,25 @@ ReactDOM.render(
 
 export default meInReact
 */
+let interests = ['JavaScript', 'React', 'Movies', 'Ice cream'];
+
+let interestElements = interests.map(i =>
+  React.createElement('li', {}, i)
+);
+
+let hOne = React.createElement('h1', {}, 'An Awesome Person');
+let p = React.createElement('p', {}, 'Who is learning React');
+let ul = React.createElement('ul', {className: "my-interests"}, interestElements);
+
+let meInReact = React.createElement('div', {className: "me"}, [
+  hOne,
+  p,
+  ul
+]);
+
+ReactDOM.render(
+  meInReact,
+  document.getElementById('global')
+);
+
+export default meInReact;
